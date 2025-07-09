bcbd07074f0ed83bb31fdc919ce64d31e19cc379
Phuc Nguyen Hoang <phuc.nguyen2@employmenthero.com>
2025-07-09 20:50:34 +0700

    Remove redundant files

--- temp/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Stage = void 0;
-var Stage;
-(function (Stage) {
-    Stage[Stage["INACTIVE"] = 0] = "INACTIVE";
-    Stage[Stage["SELECTING_LABEL"] = 1] = "SELECTING_LABEL";
-    Stage[Stage["SELECTING_DECORATION"] = 2] = "SELECTING_DECORATION";
-})(Stage || (exports.Stage = Stage = {}));
